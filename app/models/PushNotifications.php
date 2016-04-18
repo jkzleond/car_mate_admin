92beb354152113480f8878f81c91770c6c57086d
jkzleond <jkzleond@163.com>
2016-04-18 11:51:42 +0800

    haha

--- app/models/PushNotifications.php
+++ app/models/PushNotifications.php
@@ -116,8 +116,8 @@ SQL;
 SQL;
 		$bind['type_id'] = $msg_crt->type_id ? $msg_crt->type_id : 0;
 		$bind['is_read'] = $msg_crt->is_read;
-		$bind['title'] = !empty($msg_crt->content) ? $msg_crt->title.'...' : $msg_crt->title;
-		$bind['content'] = $msg_crt->content;
+        $bind['content'] = $msg_crt->content;
+		$bind['title'] = !empty($bind['content']) ? $msg_crt->title.'...' : $msg_crt->title;
 		$bind['count'] = 0;
 
 		return self::nativeExecute($sql, $bind);
